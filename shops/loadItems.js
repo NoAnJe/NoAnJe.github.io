85d29acfbec8d31cf3cbe3b88868b0a564b92321
Nolan Jessen <nolanjessen@gmail.com>
2022-04-17 21:05:12 -0500

    Testing loading from CSV

---
+++ shops/loadItems.js
@@ -0,0 +1,51 @@
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+function csvToArray(str, delimiter = ",") {
+
+    const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
+
+    const rows = str.slice(str.indexOf("\n") + 1).split("\n");
+
+    const arr = rows.map(function (row) {
+      const values = row.split(delimiter);
+      const el = headers.reduce(function (object, header, index) {
+        object[header] = values[index];
+        return object;
+      }, {});
+      return el;
+    });
+  
+    // return the array
+    return arr;
+}
+
+var text = readTextFile("mineardsItems.csv")
+
+// const reader = new FileReader();
+
+// reader.onload = function (e) {
+//     const text = e.target.result;
+//     document.write(text);
+// };
+
+// var text = reader.readAsText("mineardsItems.csv");
+
+var data = csvToArray(text);
+console.log(data);
