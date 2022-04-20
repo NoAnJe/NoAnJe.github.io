e2e92ca80b0b5056caf685f479ec9927799a0606
Nolan Jessen <nolanjessen@gmail.com>
2022-04-19 20:28:39 -0500

    Ordering form data properly

--- shops/loadItems.js
+++ shops/loadItems.js
@@ -1,5 +1,6 @@
 var allText = '';
 
+// This reads the initial JSON text file
 function readTextFile(file)
 {
     var rawFile = new XMLHttpRequest();
@@ -18,12 +19,11 @@ function readTextFile(file)
     rawFile.send(null);
 }
 
+// This function adds the label and form box for each individual item
 function addElement(value, index, array) {
-    // var saleItem = JSON.parse(value);
-    // console.log("Item" + value["Price (Iron)"])
-    // value.Item value.Quantity value.Price
     var input = document.createElement("input");
     var label = document.createElement("label");
+    linebreak = document.createElement("br");
     input.setAttribute("id", value.Item);
     label.setAttribute("id", "lbl"+value.Item);
     label.setAttribute("for", value.Item);
@@ -31,26 +31,28 @@ function addElement(value, index, array) {
     input.setAttribute("step", "1");
     input.setAttribute("max", "256");
     input.setAttribute("value", "0");
-    document.getElementById("buyForm").appendChild(input);
+    input.setAttribute("type", "number");
     document.getElementById("buyForm").appendChild(label);
+    document.getElementById("buyForm").appendChild(input);
+    document.getElementById("buyForm").appendChild(linebreak);
     var labelText = value.Item + " at " + value["Price (Iron)"] + " iron per " + value.Quantity;
     document.getElementById("lbl"+value.Item).innerHTML = labelText;
 }
 
+// Add the individual items to the shop
 const res = readTextFile("mineardsItems.json");
 var shopData = JSON.parse(allText);
 console.log(shopData)
 shopData.forEach(addElement);
-// const reader = new FileReader();
 
-// reader.onload = function (e) {
-//     const text = e.target.result;
-//     document.write(text);
-// };
+// Add an extra linebreak
+linebreak = document.createElement("br");
+document.getElementById("buyForm").appendChild(linebreak);
 
-// var text = reader.readAsText("mineardsItems.csv");
-
-// var data = csvToArray(text);
-// console.log(shopData[1].Item);
-
-// After successfully reading the file, the next step is to set each value
+// Add the submit/reset buttons
+var submit = document.createElement("input");
+submit.setAttribute("type", "submit");
+var reset = document.createElement("input");
+reset.setAttribute("type", "reset");
+document.getElementById("buyForm").appendChild(submit);
+document.getElementById("buyForm").appendChild(reset);
